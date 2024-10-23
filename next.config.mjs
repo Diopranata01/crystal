c5c5e532ed3b28f133259588247ea8cb5d72524f
dio <bagusdio01@gmail.com>
2024-10-23 13:52:42 +0800

    feat: dashboard

--- next.config.mjs
+++ next.config.mjs
@@ -2,11 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
-  basePath: '/demo/crystalkuta',
-  assetPrefix: '/demo/crystalkuta',
-  images: {
-    unoptimized: true, // Disable image optimization for static export
-  },
 };
 
 export default nextConfig;
